3c2f5197bedfaa0c59dc7442aeb8b94ac0cff3aa
rafaelhosaka <hosakarafael@gmail.com>
2024-11-19 14:20:08 +0900

    disabling csrf

--- src/main/java/com/rafaelhosaka/rhv/video/config/SecurityConfig.java
+++ src/main/java/com/rafaelhosaka/rhv/video/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -53,7 +54,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, RequestMatcher matcher) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers(matcher).permitAll());
         return http.build();
     }
